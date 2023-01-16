bece9121de2b9f57822f82a29130f04e0b8f5978
Sierra Laurent <sierraclaurent@gmail.com>
2023-01-16 15:37:30 -0800

    first commit

---
+++ .history/components/card/index_20230116150428.js
@@ -0,0 +1,16 @@
+export default function card({
+    degree = "none",
+    colour = "purple",
+    font = 0,
+    image = 'icon/personTwo.png'
+}) {
+    return(
+        <div style={{backgroundColor: colour, fontSize: font}}>
+            {
+                colour === "red" ? <img src={'icon/person.png'} width="50"/> :
+                colour === "blue" ? <img src={'icon/personThree.png'} width="50"/> :
+                                    <img src={image} width="50"/>
+            }
+        </div>
+    )
+}
