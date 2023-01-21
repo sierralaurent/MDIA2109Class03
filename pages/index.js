b5ee475a55db8b4bea536bc927c9830544bf715b
Sierra Laurent <113314536+sierralaurent@users.noreply.github.com>
2023-01-20 18:18:59 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import data from '../data/education.json'
 import { useState } from 'react'
-import Card from '../components/card'
+import Card from '../components/Card'
 import Link from 'next/link'
 
 
